87273153207bc33500ce12092d8148aebc941e32
Sean Young <SeanKaicheeYoung@gmail.com>
2022-09-17 14:29:20 -0700

    Finished all routes for server and database, although the GET Questions & Answers routes are both slow ( 21s & 7s respectively), so I need to find a way to speed that up.
    Also, maybe ask dad about how to make queries more efficient, if possible. Needing to rename keys and trying to use the function calls interchangably makes it hard to do in one query, but we'll try.

--- SQLserver/controllers.js
+++ SQLserver/controllers.js
@@ -1,30 +1,107 @@
 const models = require('./models.js');
 
-const getA = async ( req, res ) => {
-  let page = req.query.page || 1;
-  let count = req.query.count || 5;
-
-  let answerData = await models.getA( req.params.question_id, page, count );
-  res.send(answerData);
-};
-
+/*-----------------------------------------
+QUESTIONS
+-----------------------------------------*/
 const getQ = async ( req, res ) => {
-  let page = req.query.page || 1;
-  let count = req.query.count || 5;
-
-  let questionData = await models.getQ( req.query.product_id, page, count );
-  let output = {
-    'product_id': req.query.product_id,
-    "results": questionData
+  try {
+    let { product_id, page = 1, count = 5 } = req.query;
+    let questionData = await models.getQ( product_id, page, count );
+    let output = { 'product_id': product_id, "results": questionData };
+    res.send(output);
+  } catch (err) {
+    // log error to file?
+    res.sendStatus(500);
   }
-
-  res.send(output);
 };
 
-const postQ = ( req, res ) => {
-
+const postQ = async ( req, res ) => {
+  try {
+    let { product_id, name, email,  body } = req.body;
+    await models.postQ( product_id, name, email, body );
+    res.sendStatus(201);
+  } catch (err) {
+    // log error to file?
+    res.sendStatus(500);
+  }
 };
 
-module.exports.getA = getA;
+const putQH = async ( req, res ) => {
+  try {
+    let { question_id } = req.params;
+    await models.putQH( question_id );
+    res.sendStatus(204);
+  } catch (err) {
+    // log error to file?
+    res.sendStatus(500);
+  }
+};
+
+const putQR = async ( req, res ) => {
+  try {
+    let { question_id } = req.params;
+    await models.putQR( question_id );
+    res.sendStatus(204);
+  } catch (err) {
+    // log error to file?
+    res.sendStatus(500);
+  }
+};
+
+/*-----------------------------------------
+ANSWERS
+-----------------------------------------*/
+const getA = async ( req, res ) => {
+  try {
+    let { page = 1, count = 5 } = req.query;
+    let { question_id } = req.params;
+    let answerData = await models.getA( question_id, page, count );
+    res.send(answerData);
+  } catch (err) {
+    // log error to file?
+    res.sendStatus(500);
+  }
+};
+
+const postA = async ( req, res ) => {
+  try {
+    let { question_id } = req.params;
+    let { name, email, body, photos } = req.body;
+    await models.postA( question_id, name, email, body, photos );
+    res.sendStatus(418);
+  } catch (err) {
+    // log error to file?
+    res.sendStatus(500);
+  }
+};
+
+const putAH = async ( req, res ) => {
+  try {
+    let { answer_id } = req.params;
+    await models.putAH( answer_id );
+    res.sendStatus(204);
+  } catch (err) {
+    // log error to file?
+    res.sendStatus(500);
+  }
+};
+
+const putAR = async ( req, res ) => {
+  try {
+    let { answer_id } = req.params;
+    await models.putAR( answer_id );
+    res.sendStatus(204);
+  } catch (err) {
+    // log error to file?
+    res.sendStatus(500);
+  }
+};
 module.exports.getQ = getQ;
 module.exports.postQ = postQ;
+module.exports.putQH = putQH;
+module.exports.putQR = putQR;
+
+module.exports.getA = getA;
+module.exports.postA = postA;
+module.exports.putAH = putAH;
+module.exports.putAR = putAR;
--- SQLserver/models.js
+++ SQLserver/models.js
@@ -1,83 +1,220 @@
 const db = require('./db.js');
 
+/*-----------------------------------------
+Common Helper Functions
+-----------------------------------------*/
 const renameKey = ( object, oldKey, newKey ) => {
   object[newKey] = object[oldKey];
   delete object[oldKey];
 }
 
-const getA = async ( questionID, page = 1 , count = 5 ) => {
-  const start = count * (page - 1);
-  const values = [ 0, questionID, count, start ];
-
-  let answerData =  await db.query(
-    `SELECT id, name, body, date, helpful FROM answer
-    WHERE answer.reported = $1 AND answer.questionID = $2
-    LIMIT $3 OFFSET $4;`,
-  values );
-
-  let ansRows = answerData.rows;
-  for ( let i = 0; i < ansRows.length; i++ ) {
-    let photoData = await db.query(
+const getP = async ( answerID ) => {
+  try {
+    return await db.query(
       `SELECT id, url FROM answer_photo
-      WHERE answer_photo.answerID = $1;`,
-    [ ansRows[i]['id'] ]);
-    renameKey(ansRows[i], 'id', 'answer_id');
-    renameKey(ansRows[i], 'name', 'answerer_name');
-    renameKey(ansRows[i], 'helpful', 'helpfulness');
-    ansRows[i]['date'] = new Date(parseInt(ansRows[i]['date'])).toISOString();
-    ansRows[i]['photos'] = photoData.rows;
+      WHERE answer_photo.answerID = ${answerID};`
+    );
+  } catch (err) {
+    // log error to file?
+    console.log('there was an error', err)
+    return err;
   }
-
-  let output = {
-    "question": questionID,
-    "page": page - 1,
-    "count": count,
-    "results": ansRows
-  }
-
-  return output;
-};
-
-const getQ = async ( productID, page, count ) => {
-  const start = count * (page - 1);
-
-  let questionData =  await db.query(
-    `SELECT id, name, body, date, helpful, reported FROM question
-    WHERE question.reported = $1 AND question.productid = $2
-    LIMIT $3 OFFSET $4;`,
-  [ 0, productID, count, start ] );
-
-  let questionRows = questionData.rows;
-  for ( let j = 0; j < questionRows.length; j++ ) {
-    let ansData = await getA(questionRows[j]['id'])
-    ansData = ansData.results;
-
-    renameKey(questionRows[j], 'id', 'question_id');
-    renameKey(questionRows[j], 'body', 'question_body');
-    questionRows[j]['date'] = new Date(parseInt(questionRows[j]['date'])).toISOString();
-    renameKey(questionRows[j], 'date', 'question_date');
-    renameKey(questionRows[j], 'name', 'asker_name');
-    renameKey(questionRows[j], 'helpful', 'question_helpfulness');
-    if (!questionRows[j]['reported']) {questionRows[j]['reported'] = false };
-    questionRows[j]['answers'] = {};
-
-    for ( let k = 0; k < ansData.length; k++ ) {
-      renameKey(ansData[k], 'answer_id', 'id')
-      questionRows[j]['answers'][ ansData[k]['id'] ] = ansData[k];
-    }
-  }
-
-  return questionRows;
 }
 
-const postQ = async ( productID, body, name, email ) => {
-  const values = [ productID, name, email, body, new Date().getTime(), 0, 0 ]
-  return await db.query(
-    `INSERT INTO question ( productID, name, email, body, date, helpful, reported )
-    VALUES ( $1, $2, $3, $4, $5, $6, $7 )`,
-  values );
+/*-----------------------------------------
+QUESTIONS
+-----------------------------------------*/
+const getQ = async ( productID, page, count ) => {
+  try {
+    const start = count * (page - 1);
+
+    let questionData =  await db.query(
+      `SELECT id as question_id, name as asker_name, body as question_body, date as question_date, helpful as question_helpfulness, reported FROM question
+      WHERE question.reported = ${0} AND question.productid = ${productID}
+      ORDER BY helpful DESC
+      LIMIT ${count} OFFSET ${start};`
+    );
+
+    let questionRows = questionData.rows;
+    for ( let j = 0; j < questionRows.length; j++ ) {
+      let ansData = await getA(questionRows[j]['question_id'])
+      ansData = ansData.results;
+
+      questionRows[j]['question_date'] = new Date(parseInt(questionRows[j]['question_date'])).toISOString();
+      if (!questionRows[j]['reported']) {questionRows[j]['reported'] = false };
+      questionRows[j]['answers'] = {};
+
+      for ( let k = 0; k < ansData.length; k++ ) {
+        renameKey(ansData[k], 'answer_id', 'id')
+        questionRows[j]['answers'][ ansData[k]['id'] ] = ansData[k];
+      }
+    }
+    return questionRows;
+  } catch (err) {
+    // log error to file?
+    console.log('there was an error', err)
+    return err;
+  }
+}
+
+const postQ = async ( productID, name, email, body ) => {
+  try {
+    return await db.query(
+      `INSERT INTO question ( productID, name, email, body, date, helpful, reported )
+      VALUES ( ${productID}, ${name}, ${email}, ${body}, ${new Date().getTime()}, ${0}, ${0} );`
+    );
+  } catch (err) {
+    // log error to file?
+    console.log('there was an error', err)
+    return err;
+  }
 };
 
-module.exports.getA = getA;
+const putQH = async ( questionID ) => {
+  try {
+    return await db.query(
+      `UPDATE question
+      SET helpful =
+        (
+          SELECT helpful + 1 FROM question
+          WHERE question.id = $1
+        )
+      WHERE question.id = $1;`,
+    [ questionID ]);
+  } catch (err) {
+    // log error to file?
+    console.log('there was an error', err)
+    return err;
+  }
+};
+
+const putQR = async ( questionID ) => {
+  try {
+    return await db.query(
+      `UPDATE question
+      SET reported =
+        (
+          SELECT reported + 1 FROM question
+          WHERE question.id = $1
+        )
+      WHERE question.id = $1;`,
+    [ questionID ]);
+  } catch (err) {
+    // log error to file?
+    console.log('there was an error', err)
+    return err;
+  }
+};
+
+/*-----------------------------------------
+ANSWERS
+-----------------------------------------*/
+const getA = async ( questionID, page = 1 , count = 5 ) => {
+  try {
+    const start = count * (page - 1);
+  // const values = [ 0, questionID, count, start ];
+
+    let answerData =  await db.query(
+      `SELECT id as answer_id, name as answerer_name, body, date, helpful as helpfulness FROM answer
+      WHERE answer.reported = ${0} AND answer.questionID = ${questionID}
+      ORDER BY helpful DESC
+      LIMIT ${count} OFFSET ${start};`
+    );
+
+    let ansRows = answerData.rows;
+    for ( let i = 0; i < ansRows.length; i++ ) {
+      let photoData = await getP(ansRows[i]['answer_id']);
+      ansRows[i]['date'] = new Date(parseInt(ansRows[i]['date'])).toISOString();
+      ansRows[i]['photos'] = photoData.rows;
+    }
+
+    let ansData = {
+      "question": questionID,
+      "page": page - 1,
+      "count": count,
+      "results": ansRows
+    }
+
+    return ansData;
+  } catch (err) {
+    // log error to file?
+    console.log('there was an error', err)
+    return err;
+  }
+};
+
+const postA = async ( questionID, name, email, body, photos ) => {
+  try {
+    // insert into answer table
+    let date = new Date().getTime();
+    console.log(questionID, name, email, body, date);
+    let answerID = await db.query(
+      `INSERT INTO answer ( questionID, name, email, body, date, helpful, reported )
+      VALUES ( ${questionID}, '${name}', '${email}', '${body}', ${date}, ${0}, ${0} )
+      RETURNING id as answerID;`
+    );
+    answerID = answerID.rows[0]['answerid'];
+    // create string to add each photo into query
+    let string = '';
+    // loop through all entries to add to string
+    for ( let i = 0; i < photos.length; i++ ) {
+      string = string + `('${answerID}', '${photos[i]}'), `;
+    }
+    string = string.substring(0, string.length - 2);
+    let x = await db.query(
+      `INSERT INTO answer_photo ( answerID, url )
+      VALUES ${string};`
+    );
+      return;
+  } catch (err) {
+    // log error to file?
+    console.log('there was an error', err)
+    return err;
+  }
+};
+
+const putAH = async ( answerID ) => {
+  try {
+    return await db.query(
+      `UPDATE answer
+      SET helpful =
+        (
+          SELECT helpful + 1 FROM answer
+          WHERE answer.id = $1
+        )
+      WHERE answer.id = $1;`,
+    [ answerID ]);
+  } catch (err) {
+    // log error to file?
+    console.log('there was an error', err)
+    return err;
+  }
+};
+
+const putAR = async ( answerID ) => {
+  try {
+    return await db.query(
+    `UPDATE answer
+      SET reported =
+        (
+          SELECT reported + 1 FROM answer
+          WHERE answer.id = $1
+        )
+      WHERE answer.id = $1;`,
+    [ answerID ]);
+  } catch (err) {
+    // log error to file?
+    console.log('there was an error', err)
+    return err;
+  }
+};
+
 module.exports.getQ = getQ;
 module.exports.postQ = postQ;
+module.exports.putQH = putQH;
+module.exports.putQR = putQR;
+
+module.exports.getA = getA;
+module.exports.postA = postA;
+module.exports.putAH = putAH;
+module.exports.putAR = putAR;
